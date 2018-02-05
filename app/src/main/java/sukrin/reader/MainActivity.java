6ce25e8f8c5ee85e07599d75d5d022016340a24e
ukrin <sukrin02@gmaill.cm>
2018-02-05 07:06:29 +0700

    newbie

---
+++ app/src/main/java/sukrin/reader/MainActivity.java
@@ -0,0 +1,13 @@
+package sukrin.reader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/sukrin/reader/PDFDoc.java
@@ -0,0 +1,28 @@
+package sukrin.reader;
+
+/**
+ * Created by SUKRON on 11/3/2017.
+ */
+
+public class PDFDoc {
+    String name,path;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
+
